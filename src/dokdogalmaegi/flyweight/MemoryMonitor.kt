a84f160d69e675ca7e798b62a1adae22b37fac68
dokdogalmaegi <dokdogalmaegi@gmail.com>
2025-01-07 09:41:20 +0900

    Add memory monitor

--- src/dokdogalmaegi/flyweight/MemoryMonitor.kt
+++ src/dokdogalmaegi/flyweight/MemoryMonitor.kt
@@ -1,9 +1,14 @@
 package dokdogalmaegi.flyweight
 
 object MemoryMonitor {
+    private const val MAX_MEMORY = 1000
     private var usedMemory = 0
 
     fun allocateMemory(memory: Int) {
+        if (usedMemory + memory > MAX_MEMORY) {
+            throw OutOfMemoryError("Not enough memory, used: $usedMemory, requested: $memory($usedMemory + $memory)")
+        }
+
         usedMemory += memory
     }
 }
--- src/dokdogalmaegi/flyweight/TextStyleFactory.kt
+++ src/dokdogalmaegi/flyweight/TextStyleFactory.kt
@@ -7,10 +7,12 @@ object TextStyleFactory {
         return "$font[]$size[]$color"
     }
 
-    fun getStyle(font: String, size: Int, color: TextColor): TextStyle {
-        return styles.computeIfAbsent(getStyleKey(font, size, color)) {
+    fun getStyle(font: String, size: Int, color: TextColor): TextStyle =
+        styles.computeIfAbsent(getStyleKey(font, size, color)) {
             println("Creating new TextStyle")
-            TextStyle(font, size, color)
+            val textStyle = TextStyle(font, size, color)
+            MemoryMonitor.allocateMemory(textStyle.toString().length)
+
+            textStyle
         }
-    }
 }
